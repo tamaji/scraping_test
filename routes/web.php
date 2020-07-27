be03040d793ab66c121472f989c80dd5bec0c955
tamaji <54011596+tamaji@users.noreply.github.com>
2020-07-27 11:38:30 +0900

    スクレイピング部分追加
    ルーティングする部分に直接スクレイピング部分を追加しています。

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use DOMWrap\Document;
+use HeadlessChromium\BrowserFactory;
+use GuzzleHttp\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+// ここに書かれている URL はダミーです
+// クロールリストは実際にはデータベースから取ってくるとか
+$sites = [
+    [
+        'url' => 'https://books.rakuten.co.jp/search?g=101&merch=94142&v=1&s=1&spv=2&h=100&l-id=ebook-event-all-8949-11',
+        'selector' => '.rbcomp__item-tile__list > li > div > h3 > a > span',
+    ],
+];
+
+foreach ($sites as $site) {
+    $browserFactory = new BrowserFactory(
+        // 実行するブラウザのパスに応じて変更
+        // https://github.com/chrome-php/headless-chromium-php/issues/75
+        //'/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+        'chromium-browser'
+    );
+    $browser = $browserFactory->createBrowser();
+    $page = $browser->createPage();
+    $page->navigate($site['url'])->waitForNavigation();
+    $evaluation = $page->evaluate('document.documentElement.innerHTML');
+    $value = $evaluation->getReturnValue();
+    $browser->close();
+
+    $doc = new Document;
+    $node = $doc->html($value);
+
+    // 取得した文字列
+    $text = $node->find($site['selector'])->text();
+
+    // 通知したりデータベースに追加したり
+    echo $text, PHP_EOL;
+}
+
+//$sites = [
+//    [
+//        'url' => 'https://books.rakuten.co.jp/search?g=101&merch=94142&v=1&s=1&spv=2&h=100&l-id=ebook-event-all-8949-11',
+//        'selector' => '.rbcomp__item-tile__list > li > div > h3 > a > span',
+//    ],
+//];
+
+//$client = new Client;
+
+//foreach ($sites as $site) {
+//    $response = $client->get($site['url']);
+//    $html = (string) $response->getBody();
+//    $doc = new Document;
+//    $node = $doc->html($html);
+
+    // 取得した文字列
+//    $text = $node->find($site['selector'])->text();
+
+    // 通知したりデータベースに追加したり
+//    echo $text, PHP_EOL;
+//}
